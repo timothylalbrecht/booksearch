72f574b9b7b00e22a6eadde6923ac2640f592497
timmy burrito <timothylalbrecht@gmail.com>
2019-02-03 22:55:34 -0500

    pages and comps

---
+++ client/src/components/SearchForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SearchForm = ({onSubmit, onChange, value}) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <input 
+        placeholder="Search by Title, Author, or Subject" 
+        className="form-control border-success mb-2"
+        type="text"
+        onChange={onChange}
+        value={value}
+        name="searchTerm"
+      />
+      <button 
+        className="btn btn-block btn-outline-dark"
+        onClick={onSubmit}
+        >
+        Search
+      </button>
+    </form>
+  )
+}
+
+export default SearchForm;
