d85e9eac031eb2c06a5ccec74eb4cb86835e1cb9
timmy burrito <timothylalbrecht@gmail.com>
2019-02-05 22:08:04 -0500

    cleaned comments

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -7,7 +7,7 @@ class Saved extends Component {
     books: []
   }
 
-  // retrieve saved books on load
+  // get saved books
   componentDidMount() {
     this.getBooks();
   }
@@ -18,7 +18,7 @@ class Saved extends Component {
       .catch(err => console.log(err));
   }
 
-  // handle deleting a book
+  // delete a book
   deleteBook = (bookId) => {
     API.deleteBook(bookId)
       .then(this.getBooks)
@@ -33,7 +33,7 @@ class Saved extends Component {
         </div>
         <div className="container-fluid">
           <div className="row align-items-stretch">
-            {/* use ternary to check if books are in state */}
+            {/* books in state? */}
 
             {!this.state.books.length
               ? (
@@ -49,9 +49,7 @@ class Saved extends Component {
                         <h5 className="card-header">{book.title}</h5>
                         <img src={book.image} alt={book.title} className="card-img"/>
                         <div className="card-body">
-                          <h6 className="card-subtitle">By {book
-                              .authors
-                              .join(" & ")}</h6>
+                          <h6 className="card-subtitle">By {book.authors.join(" & ")}</h6>
                           <p>{book.description}</p>
                           <div className="btn-group" role="group">
                             <button type="button" className="btn" onClick={() => this.deleteBook(book._id)}>Delete Book</button>
@@ -62,7 +60,7 @@ class Saved extends Component {
                     </div>
                   )
                 })
-}
+            }
           </div>
         </div>
       </div>
